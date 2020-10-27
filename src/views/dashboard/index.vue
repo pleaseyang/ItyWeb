e4d4275466a1706ed9f551d43dddd4b033059acd
杨洋 <tjbdyangyang@163.com>
2020-10-27 14:23:13 +0800

    去除dashboard的websocket

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,11 +2,7 @@
   <div />
 </template>
 <script>
-import { createSocket, sendWSPush } from '@/utils/websocket'
 export default {
-  name: 'Dashboard',
-  created() {
-    createSocket()
-  }
+  name: 'Dashboard'
 }
 </script>
