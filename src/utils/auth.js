679c20b221da51c21ba178fd7af95ad7dc53cdf0
杨洋 <tjbdyangyang@163.com>
2021-01-04 16:59:56 +0800

    set token cookie expires

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,7 @@ export function getToken() {
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, { expires: 30 })
 }
 
 export function removeToken() {
