fa32f60d787102e9bc316c12989d46604fe78039
杨洋 <tjbdyangyang@163.com>
2021-03-19 14:43:30 +0800

    request 添加断网与延迟提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -54,55 +54,65 @@ service.interceptors.response.use(
     return response.data
   },
   error => {
-    const { data } = error.response
-    if (data.code === 430) {
-      MessageBox.confirm(data.message, '', {
-        type: 'warning'
-      }).then(() => {
-        removeToken()
-        location.reload()
-      })
-    } else if (data.code === 401) {
-      const config = error.response.config
-      if (!isRefreshing) {
-        isRefreshing = true
-        return refreshToken().then(res => {
-          const { access_token } = res
-          setToken(access_token)
-          config.headers['Authorization'] = 'Bearer ' + access_token
-          config.baseURL = ''
-          requests.forEach(cb => cb(access_token))
-          requests = []
-          return service(config)
-        }).finally(() => {
-          isRefreshing = false
+    if (error.response !== undefined) {
+      const { data } = error.response
+      if (data.code === 430) {
+        MessageBox.confirm(data.message, '', {
+          type: 'warning'
+        }).then(() => {
+          removeToken()
+          location.reload()
+        })
+      } else if (data.code === 401) {
+        const config = error.response.config
+        if (!isRefreshing) {
+          isRefreshing = true
+          return refreshToken().then(res => {
+            const { access_token } = res
+            setToken(access_token)
+            config.headers['Authorization'] = 'Bearer ' + access_token
+            config.baseURL = ''
+            requests.forEach(cb => cb(access_token))
+            requests = []
+            return service(config)
+          }).finally(() => {
+            isRefreshing = false
+          })
+        } else {
+          // 正在刷新token，将返回一个未执行resolve的promise
+          return new Promise((resolve) => {
+            // 将resolve放进队列，用一个函数形式来保存，等token刷新后直接执行
+            requests.push((token) => {
+              config.headers['Authorization'] = 'Bearer ' + token
+              config.baseURL = ''
+              resolve(service(config))
+            })
+          })
+        }
+      } else if (data.code === 500) {
+        Notification.error({
+          title: data.message,
+          dangerouslyUseHTMLString: true,
+          message: `<p>ErrorNumber:${data.data.errorId}</p> We'll deal with it as soon as possible`,
+          duration: 0
         })
       } else {
-        // 正在刷新token，将返回一个未执行resolve的promise
-        return new Promise((resolve) => {
-          // 将resolve放进队列，用一个函数形式来保存，等token刷新后直接执行
-          requests.push((token) => {
-            config.headers['Authorization'] = 'Bearer ' + token
-            config.baseURL = ''
-            resolve(service(config))
-          })
+        Message({
+          message: data.message,
+          type: 'error',
+          duration: 5 * 1000
         })
       }
-    } else if (data.code === 500) {
-      Notification.error({
-        title: data.message,
-        dangerouslyUseHTMLString: true,
-        message: `<p>ErrorNumber:${data.data.errorId}</p> We'll deal with it as soon as possible`,
-        duration: 0
-      })
+      return Promise.reject(error)
     } else {
-      Message({
-        message: data.message,
+      MessageBox.alert(error.message, error.name, {
         type: 'error',
-        duration: 5 * 1000
+        closeOnClickModal: false,
+        closeOnPressEscape: false,
+        closeOnHashChange: false,
+        roundButton: true
       })
     }
-    return Promise.reject(error)
   }
 )
 
