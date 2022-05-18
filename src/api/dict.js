3606ebbd7d9c6f714222cdcecff1b67907b75c4c
杨洋 <tjbdyangyang@163.com>
2022-05-18 09:27:24 +0800

    字典获取

--- src/api/dict.js
+++ src/api/dict.js
@@ -95,3 +95,11 @@ export function dictDataListClass(data) {
     data
   })
 }
+
+export function dictDataSelect(data) {
+  return request({
+    url: '/dictData/select',
+    method: 'post',
+    data
+  })
+}
---
+++ src/components/DictTag/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-tag :type="dictDataTag.list_class">
+    {{ dictDataTag.label }}
+  </el-tag>
+</template>
+
+<script>
+export default {
+  name: 'DictTag',
+  props: {
+    dictTypeId: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    dictData: {
+      type: Array,
+      default: () => {
+        return []
+      },
+      required: true
+    },
+    value: {
+      type: [String, Number],
+      default: null,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dictDataTag: {
+        default: 0,
+        dict_type_id: this.dictTypeId,
+        label: '',
+        list_class: null,
+        sort: 0,
+        value: ''
+      }
+    }
+  },
+  watch: {
+    value() {
+      this.set()
+    }
+  },
+  created() {
+    this.set()
+  },
+  methods: {
+    set() {
+      const value = this.value.toString()
+      let dictData = this.dictData.filter((e) => e.dict_type_id === this.dictTypeId).find((e) => e.value === value)
+      if (dictData === undefined) {
+        dictData = {
+          default: 0,
+          dict_type_id: this.dictTypeId,
+          label: value,
+          list_class: null,
+          sort: 0,
+          value: value
+        }
+      }
+      this.dictDataTag = dictData
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
